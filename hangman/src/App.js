df0ac5720677210dc3dac304c85ee2e4f41c45a2
dinythomas89 <dinythomas89@gmail.com>
2021-12-06 13:11:13 +0100

    added hangman files

---
+++ hangman/src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+import Word from "./components/Word";
+import WrongLetters from "./components/WrongLetters";
+import Notification from "./components/Notification";
+import Popup from "./components/Popup";
+import { showNotification as show } from "./helpers/helpers";
+import { words } from "./helpers/words";
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((correctLetters) => [...correctLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+    setCorrectLetters([]);
+    setWrongLetters([]);
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetter={correctLetters} />
+      </div>
+      <Popup
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        selectedWord={selectedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notification showNotification={showNotification} />
+    </div>
+  );
+}
+
+export default App;
