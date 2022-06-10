3c678811cc7b3403a0eacbd9e67ee263f957e147
dinythomas89 <dinythomas89@gmail.com>
2022-06-10 12:59:20 +0200

    created the portfolio for my projects

---
+++ Desktop/portfolio/src/App.js
@@ -0,0 +1,23 @@
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Home from "./Components/Home/Home";
+import Projects from "./Components/Projects/Projects";
+import Skills from "./Components/Skills/Skills";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
